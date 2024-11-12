ae6998dd0f1bf71ab87a26812a8586617b1184d0
Anagamedina <anaga.medina@gmail.com>
2024-11-12 18:15:19 +0100

    split_quote mejorado norminette

--- src/tokenize/token_utils.c
+++ src/tokenize/token_utils.c
@@ -23,30 +23,45 @@ static char *ft_strncpy(char *s1, char *s2, int n)
 	s1[i] = '\0';
 	return (s1);
 }
-
-static int	count_words(char *str)
+/*
+ * Esta función se encarga de saltar entre comillas simples o dobles.
+ *
+ * Desde una posicion especifica cuando se encuenta las QUOTES
+ * me vas a recorrer la i(puntero) hasta hacer match con la quote
+ * Retorna 1 para indicar que se encontró una palabra
+ * */
+static int skip_quotes(char *str, int *i)
 {
-	int i = 0;
-	int wc = 0;
-	int in_quotes = 0;
 	char quote;
 
+	quote = str[*i];
+	(*i)++;
+	while (str[*i] && str[*i] != quote)
+		(*i)++;
+	if (str[*i] == quote)
+		(*i)++;
+	return (1);
+}
+
+/*Cuenta cuántas "palabras" hay en la cadena str,
+ * tomando en cuenta las comillas
+ */
+static int	count_words(char *str)
+{
+	int i;
+	int wc;
+
+	i = 0;
+	wc = 0;
 	while (str[i])
 	{
 		while (str[i] && (str[i] == ' ' || str[i] == '\t' || str[i] == '\n'))
 			i++;
 		if (str[i] == '\'' || str[i] == '"')
 		{
-			quote = str[i++];
-			in_quotes = 1;
-			while (str[i] && str[i] != quote)
-				i++;
-			if (str[i] == quote)
-				in_quotes = 0;
-			i++; // Saltar la comilla de cierre
-			wc++;
+			wc += skip_quotes(str, &i);
 		}
-		else if (str[i] && !in_quotes)
+		else if (str[i])
 		{
 			wc++;
 			while (str[i] && (str[i] != ' ' && str[i] != '\t' && str[i] != '\n'))
@@ -56,38 +71,71 @@ static int	count_words(char *str)
 	return (wc);
 }
 
+/*
+Esta función salta los espacios,
+tabulaciones y saltos de línea.
+ */
+static void skip_whitespace(char *str, int *i)
+{
+	while (str[*i] && (str[*i] == ' ' || str[*i] == '\t' || str[*i] == '\n'))
+		(*i)++;
+}
+
+
+/*Se encarga de copiar una palabra (definida entre espacios,
+ * tabulaciones o comillas) en la posición de la lista out*/
+static int copy_word(char **out, char *str, int start, int end, int *k)
+{
+	out[*k] = (char *)malloc(sizeof(char) * (end - start + 1));
+	if (!out[*k])
+		return (-1);
+	ft_strncpy(out[(*k)++], &str[start], end - start);
+	return (0);
+}
+
+/*Inicializa las variables necesarias antes de
+ * comenzar el procesamiento de la cadena*/
+static int init_vars_split(int *i, int *k, int *wc, char ***out, char *str)
+{
+	*i = 0;
+	*k = 0;
+	*wc = count_words(str);
+	*out = (char **)malloc(sizeof(char *) * (*wc + 1));
+	if (!*out)
+		return (-1);
+	return (0);
+}
+
+
+
+/*Es la función principal que utiliza
+las otras funciones para dividir la cadena
+ en palabras, considerando las comillas.
+ */
+
+//**********MAIN FUNCTION***************/
 char **ft_split_quote(char *str)
 {
-	int i = 0,
-	start = 0,
-	end = 0,
-	k = 0;
-	int wc = count_words(str);
-	char **out = (char **)malloc(sizeof(char *) * (wc + 1));
+	int i;
+	int j;
+	int k;
+	int wc;
+	char **out;
+
+	if (init_vars_split(&i, &k, &wc, &out, str) == -1)
+		return (NULL);
 
 	while (str[i])
 	{
-		while (str[i] && (str[i] == ' ' || str[i] == '\t' || str[i] == '\n'))
-			i++;
-		start = i;
+		skip_whitespace(str, &i);
+		j = i;
 		if (str[i] == '\'' || str[i] == '"')
-		{
-			char quote = str[i++];
-			while (str[i] && str[i] != quote)
-				i++;
-			i++; // Saltar la comilla de cierre
-		}
+			skip_quotes(str, &i);
 		else
-		{
 			while (str[i] && (str[i] != ' ' && str[i] != '\t' && str[i] != '\n'))
 				i++;
-		}
-		end = i;
-		if (end > start)
-		{
-			out[k] = (char *)malloc(sizeof(char) * (end - start + 1));
-			ft_strncpy(out[k++], &str[start], end - start);
-		}
+		if (i > j && copy_word(out, str, j, i, &k) == -1)
+			return (NULL);
 	}
 	out[k] = NULL;
 	return (out);
