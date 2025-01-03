987d626065863d9b0b1e0a6a7ee4f40fd0b83e60
Daruuu <thedaruny16@gmail.com>
2025-01-03 11:08:55 +0100

    add new todos :)

--- src/parser/parser_syntax_expand.c
+++ src/parser/parser_syntax_expand.c
@@ -6,7 +6,7 @@
 /*   By:  dasalaza < dasalaza@student.42barcel>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/17 11:26:47 by catalinab         #+#    #+#             */
-/*   Updated: 2025/01/02 15:48:24 by  dasalaza        ###   ########.fr       */
+/*   Updated: 2025/01/03 11:08:29 by  dasalaza        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,7 @@ static int	handle_backslash_after_dollar(t_tokens *token)
 }
 
 // caso $'..'---> \t ... SIN COMILLA DOBLES
+// TODO: rename function
 static int	handle_single_quotes_after_dollar(t_tokens *token)
 {
 	char	*temp;
@@ -253,33 +254,48 @@ void	handle_dollar_cases(t_tokens *token, t_list *env_list)
 
 	/**
 	 * Case: "'$...'"
-	 * Expands variables inside double quotes while preserving single quotes.
-	 * Steps:
+	 * this 3 cases are valid:
+	 * " ' $USER ' "
+	 * ""' $USER ' ""
+	 * " " " ' $USER ' " " "
+	 * 
+	 * Steps: 
+	 * Expands variables inside d_quotes while preserving single quotes.
 	 * Remove outer double quotes using remove_quotes_str.
 	 * Expand variables, skip data inside single quotes
-	 * " ' $USER ' "
 	 */
+	// TODO: rename function
 	if (check_double_simple_dollar_case(token->str))
 	{
+		printf("original str: [%s]\n", token->str);
+
 		tmp = remove_quotes_str(token->str, D_QUOTE);
-		
 		token->str = ft_strdup(tmp);
 		printf("after remove_quotes_str: [%s]\n", token->str);
 
 		res = replace_dollar_variable_skip_s_quote(token->str, env_list);
 		token->str = ft_strdup(res);
 		printf("after replace_dollar_variable : [%s]\n", token->str);
+
 		free(tmp);
+
 		return ;
 	}
 
 	/**
-	 * Case: "$ '...'"
+	 * Case: "$'...'"
+	 * Case: " $ '...'"
+	 * Case: " $ $ '...'"
+	 * Error case: " $$ '...'"
+	 * 
 	 * Steps:
-	 * Detected by check_dollar_with_space_single.
-	 * Managed by handle_dollar_with_space_single.
-	 * "$ 'USER ' "
+	 * the dollar between d_quote and s_quote
+	 * only one d_quote (open and close) 
+	 * can have spaces between d_quote and s_quote
+	 * can't have 2 consecutives dollar signs ($$) 
 	 */
+
+	// TODO: rename function
 	if (check_dollar_with_space_single(token->str))
 	{
 		handle_dollar_with_space_single(token);
@@ -292,6 +308,7 @@ void	handle_dollar_cases(t_tokens *token, t_list *env_list)
 	 * Remove outer double quotes using remove_quotes_str.
 	 * Copy the modified string back to token->str.
 	 */
+	/*
 	if (check_d_quote_dollar_s_quote(token->str))
 	{
 		tmp = remove_quotes_str(token->str, D_QUOTE);
@@ -299,19 +316,24 @@ void	handle_dollar_cases(t_tokens *token, t_list *env_list)
 		free(tmp);
 		return ;
 	}
+	*/
 
 	/**
-	 * Case: "$USER"
-	 * @details: Expands a simple environment variable.
-	 * Input: "$USER" Output: daruuu (assuming $USER = daruuu).
+	 * Case: "$'USER"
+	 * Case: "this is the paht: $PATH"
+	 * 
 	 * Steps:
+	 * Expands a simple environment variable.
 	 * Detected by check_dollar_simple.
 	 * Managed by handle_single_quotes_after_dollar
 	 */
+	/*
 	if (check_dollar_simple(token->str))
 	{
 		handle_single_quotes_after_dollar(token);
 	}
+	*/
+
 	/**
 	 * Case: No expansion required
 	 * @details: Handles tokens that should not trigger variable expansion.
@@ -323,8 +345,8 @@ void	handle_dollar_cases(t_tokens *token, t_list *env_list)
 	if (handle_no_expand_cases(token) == FALSE)
 	{
 		/**
-		 * Default Case: General dollar expansion
-		 * Description: Expands variables based on env_list if none of the above cases are matched.
+		 * Case: General dollar expansion
+		 * Expands variables based on env_list if none of the above cases are matched.
 		 * @example:
 		 * Input: "$HOME/$USER"
 		 * Output: /home/daruu/daruu (assuming $HOME=/home/daruu and $USER=daruu).
