57afc39d0a62b4aa8e8faee0c511b2b4a2f3afbb
Anagamedina <anaga.medina@gmail.com>
2025-01-24 11:45:40 +0100

    ideas para empezar  a factorizar

--- src/parser/parser_expand_cases.c
+++ src/parser/parser_expand_cases.c
@@ -11,6 +11,7 @@ int	has_consecutives_env_variables_in_token(t_tokens *token)
 	
 	count_dollar = 0;
 	i = 0;
+
 	if (token->str[0] == D_QUOTE)
 		i = 1;
 	while (token->str[i] != '\0')
@@ -38,7 +39,87 @@ int	has_consecutives_env_variables_in_token(t_tokens *token)
 }
 
 
+
 char *expand_consecutives_variables(t_tokens *token, t_list *env_list)
+{
+    int i; 
+	int j;
+	int len;
+    char *tmp = NULL;
+    char *find_value = NULL;
+    char *result = ft_strdup(""); 
+    char *token_updated = NULL;
+
+	i = 0; 
+	j = 0;
+	len = 0;
+
+    if (!token || !token->str || !env_list)
+        return (NULL);
+
+    while (token->str[i] != '\0')
+    {
+        if (token->str[i] == DOLLAR_SIGN)
+        {
+            i++; // Salta el '$'
+            j = i;
+
+            if (token->str[j] == '\0' || token->str[j] == SPACE)
+            {
+				//1)primer caso, nos encontramos un dolla solo 
+                tmp = ft_strdup("$"); // Agrega el dólar solo al resultado
+                token_updated = ft_strjoin(result, tmp);
+                free(result);
+                free(tmp);
+                result = token_updated;
+                continue;
+            }
+
+            while (token->str[j] != '\0' && token->str[j] != DOLLAR_SIGN && token->str[j] != SPACE)
+                j++;
+
+            len = j - i;
+            tmp = ft_substr(token->str, i, len); // Extrae el nombre de la variable
+			//2)segundo caso devolvemos temp y la usamos en el siguiente caso 
+            find_value = find_value_in_env(env_list, tmp);
+            free(tmp);
+
+            if (find_value)
+            {
+                token_updated = ft_strjoin(result, find_value);
+                free(result);
+                result = token_updated;
+            }
+          	else	
+			{
+				i = j;
+				continue ;
+
+			}
+			i = j; // Salta al final del nombre de la variable
+        }
+        else
+        {	//3)tercer caso si no encontramos un dollar lo que hacemos en agregarlo en la cajita 
+            tmp = ft_substr(token->str, i, 1); // Extrae un carácter
+            token_updated = ft_strjoin(result, tmp);
+            free(result);
+            free(tmp);
+            result = token_updated;
+            i++;
+        }
+    }
+
+    return (result);
+}
+
+
+
+
+
+
+
+//COPIA
+/*char *expand_consecutives_variables(t_tokens *token, t_list *env_list)
 {
     int i = 0, j = 0, len = 0;
     char *tmp = NULL;
@@ -104,3 +185,4 @@ char *expand_consecutives_variables(t_tokens *token, t_list *env_list)
     return (result);
 }
 
+*/
