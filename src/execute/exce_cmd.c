91365be0c1a45f9e7719c168f053f0bc02c7c68b
daruuu <thedaruny16@gmail.com>
2025-02-25 10:01:48 +0100

    limpiando comentarios, caso resuelto de pre_executor pero falta un caso redireccion sin comandos

--- src/execute/exce_cmd.c
+++ src/execute/exce_cmd.c
@@ -48,40 +48,6 @@ void	handle_parent(t_cmd *curr_cmd, int *pipe_fd, int *input_fd)
 }
 
 
-/*void execute_builtin_with_redirects(t_mini *mini, t_cmd *curr_cmd)
-{
-
-	//heredoc(curr_cmd);
-	if (apply_redirections(curr_cmd) > 0)
-	{
-		if (curr_cmd->input_fd != STDIN_FILENO)
-			redirect_in(curr_cmd->input_fd);
-		if (curr_cmd->output_fd != STDOUT_FILENO)
-			redirect_out(curr_cmd->output_fd);
-	}
-	else
-	{
-		saved_stdout = dup(STDOUT_FILENO);  // Guardar stdout original
-		if (saved_stdout == -1)
-			return (perror("Error guardando stdouteooof"), FALSE);
-		if (dup2(curr_cmd->output_fd, STDOUT_FILENO) == -1)
-			return (perror("Error redirigiendo salidaeooof"), FALSE);
-		close(curr_cmd->output_fd);  // Cerrar el FD de salida original
-	}
-
-	// Ejecutar el builtin normalmente
-	cases_builtins(mini, curr_cmd);
-
-	// return (TRUE);
-}
-*/
-
-// int execute_builtin_with_redirects(t_mini *mini, t_cmd *curr_cmd)
-// {
-// 	cases_builtins(mini, curr_cmd);
-// 	return (TRUE);
-// }
-
 int pre_executor(t_mini *mini, t_cmd *cmd)
 {
 	int saved_stdout = -1;
@@ -96,7 +62,7 @@ int pre_executor(t_mini *mini, t_cmd *cmd)
 		return (TRUE);
 	}
 	if (apply_redirections(cmd) == -1)
-		return (FALSE);  // Si falla la redirección, no ejecutar el builtin
+		return (FALSE);
 
 	if (cmd->output_fd != STDOUT_FILENO)
 	{
@@ -147,59 +113,3 @@ int	execute_commands(t_mini *mini)
 	wait_children(mini);
 	return (TRUE);
 }
-
-
-/*
-int	execute_commands(t_mini *mini)
-{
-	t_list	*t_list_exec_cmd;
-	t_cmd	*curr_cmd;
-	pid_t	pid;
-	int		input_fd;
-	int		pipe_fd[2];
-	int		i;
-	int		status;
-
-	i = 0;
-	input_fd = STDIN_FILENO;
-	t_list_exec_cmd = mini->exec->first_cmd;
-	while (t_list_exec_cmd)
-	{
-		curr_cmd = (t_cmd *)t_list_exec_cmd->content;
-		curr_cmd->cmd_id = i++;
-
-		if (curr_cmd->is_builtin == 1 && t_list_exec_cmd->next == NULL)
-		{
-			cases_builtins(mini, curr_cmd);
-			return (TRUE);
-		}
-		setup_fds(curr_cmd, pipe_fd, &input_fd);
-		pid = fork();
-		if (pid < 0)
-		{
-			perror("Error creando proceso hijo");
-			exit(EXIT_FAILURE);
-		}
-		if (pid == 0)
-		{
-			handle_child(curr_cmd, mini);
-			//printf("Mi PID hijo es: %d\n", getpid());
-			exit(EXIT_SUCCESS);
-		}
-		else
-		{
-			handle_parent(curr_cmd, pipe_fd, &input_fd);
-			//printf("Mi PID padre es: %d\n", getpid());
-		}
-		t_list_exec_cmd = t_list_exec_cmd->next;
-	}
-
-	// wait all children process
-	while (waitpid(-1, &status, 0) > 0)
-	{
-		if (WIFEXITED(status))
-			mini->exit_status = WEXITSTATUS(status);
-	}
-
-	return (TRUE);
-}*/
