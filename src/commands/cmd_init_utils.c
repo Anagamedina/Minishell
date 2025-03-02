6259f633f0bd8495d04a16270c585fa81a71b03e
daruuu <thedaruny16@gmail.com>
2025-03-02 22:54:43 +0100

    add frees

--- src/commands/cmd_init_utils.c
+++ src/commands/cmd_init_utils.c
@@ -6,7 +6,7 @@
 /*   By: anamedin <anamedin@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/19 15:45:37 by anamedin          #+#    #+#             */
-/*   Updated: 2025/02/19 15:47:06 by anamedin         ###   ########.fr       */
+/*   Updated: 2025/03/02 20:23:02 by dasalaza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ t_cmd	*process_command_token(t_tokens *token, char **paths, int *cmd_id)
 	new_cmd = create_new_command(token, paths);
 	if (!new_cmd)
 	{
-		printf("Error: Falló la creación del comando '%s'.\n", token->str);
+		printf("Error: Fail creation command '%s'.\n", token->str);
 		return (NULL);
 	}
 	new_cmd->cmd_id = (*cmd_id)++;
@@ -44,10 +44,20 @@ void	add_command_to_list(t_list **cmd_list, t_tokens *token, \
 
 	cmd = process_command_token(token, paths, cmd_id);
 	if (!cmd)
-		return (free_cmd_list (*cmd_list), (void) NULL);
+	{
+		// return (free_cmd_list (*cmd_list), (void) NULL);
+		free_cmd_list (*cmd_list);
+		return ;
+	}
 	new_node = ft_lstnew(cmd);
 	if (!new_node)
-		return (free_command(cmd), free_cmd_list(*cmd_list), (void) NULL);
+	{
+		// return (free_command(cmd), free_cmd_list(*cmd_list), (void) NULL);
+		free_command(cmd);
+		// free_cmd_list(*cmd_list);
+		//*cmd_list = NULL;
+		return ;
+	}
 	ft_lstadd_back(cmd_list, new_node);
 }
 
---
+++ test_echo_chars_escape.sh
@@ -0,0 +1,7 @@
+echo "Hola \$VAR"
+echo 'Hola \$VAR'
+echo "Hola \"Mundo\""
+echo 'Hola \'Mundo\''
+echo "Hola \\ Mundo"
+echo 'Hola \\ Mundo'
+
